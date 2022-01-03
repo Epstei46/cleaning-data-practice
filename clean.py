9de8c5370ea6f366c82ead4d1dca5cb543062fed
Steven Epstein <sbepstein42@gmail.com>
2022-01-03 13:35:55 -0500

    Initial Commit

---
+++ clean.py
@@ -0,0 +1,14 @@
+# This is the file where I will be practicing data manipulation alongside the Cleaning Data videos on PluralSight
+# # Exercise setup/outline from https://ed.devmountain.com/materials/data-bp-1/exercises/cleaning-data/.
+
+
+# STEP 1: UNDERSTANDING YOUR DATA
+
+
+# STEP 2: REMOVE AND FIX COLUMNS
+
+
+# STEP 3: INDEX AND FILTER
+
+
+# STEP 4: HANDLE BAD DATA
