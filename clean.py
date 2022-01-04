609d104a18be97ab8259bebe03e7db1aca6f0d8f
Steven Epstein <sbepstein42@gmail.com>
2022-01-04 17:51:28 -0500

    Step 3 Completed

--- clean.py
+++ clean.py
@@ -8,6 +8,7 @@ data = pd.read_csv('artwork_sample.csv')
 fulldf = pd.read_csv('artwork_data.csv', low_memory=False)
 
 
+
 ###########################################
 # STEP 1: UNDERSTANDING YOUR DATA
 ###########################################
@@ -57,10 +58,28 @@ data.rename(columns={'thumbnailUrl': 'thumbnail'}, inplace=True) # this renames 
 # Could also do data.columns, copy list of column names, make changes in that list, then data.columns = that list.
 
 
+
 ###########################################
 # STEP 3: INDEX AND FILTER
 ###########################################
+data['id'][1] # displays first value in that series
+data[1:5][['artist', 'title']] # NOTE displays rows at integer position 1,2,3,4, ignores labels, & only displays specified columns
 
+data.loc[0:2, 'title'] # displays series from that column
+data.loc[0:2, :] # NOTE displays rows at label 0,1,2 & ALL columns
+data.loc[[1,5], ['artist', 'title']] # displays both rows/columns as dataFrame
+data.loc[0:2, 'artist':'artistId'] # displays dataFrame with all columns between
+data.loc[data.artist == 'Blake, Robert', ['artist', 'title']] # only rows with that name & only specified columns in dataFrame
+
+data.iloc[0:3, :] # NOTE displays rows at integer position 0,1,2, ignores labels
+data.iloc[[5,8], [0,2]] # displays rows at integer position 5,8 and columns at integer position 0,2. Integer position is the index, not the column,so  displayed anyways
+
+data.set_index('id', inplace=True) # so index is 'id' instead of integer position
+data.iloc[0:3, :] is data.loc[1035:1037, :] # without the above line, this would return an empty dataFrame and they would not be the same
+
+data.medium # 'medium' is a column name, this displays a series of strings, which allows us to use string methods
+data.medium.str.contains('graphite|line', case=False, regex=True) # return series of True/False, case sensitive by default, and made it regex so I could use logical OR operator |
+data.loc[data.medium.str.contains('graphite', na=False, case=False), ['artist','medium']]	# displays dataFrame with rows where True, only shows above 2 columns. na=False tells it to skip NA/NaN values, case=False tells it to be case insensitive.
 
 
 
